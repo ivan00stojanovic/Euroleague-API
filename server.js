f02d3d67209f10ffb3349255d0d7ea2d9ecc3da6
Ivan Stojanovic <ivanstojanovic00@gmail.com>
2023-07-13 19:26:23 -0400

    skelet solved, back at it tmrw

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const app = express()
-PORT = 1991
+const dotenv = require('dotenv')
+const connectDB = require('./config/db')
+// PORT = 1991
+
+// Load config
+dotenv.config({path: './config/config.env'})
+
+connectDB()
 
 app.get('/', (req,res) => {
     res.send("Working")
 })
 
+const PORT = process.env.PORT || 1991
+
+
 app.listen(PORT, () => {
-    console.log('Listening on PORT ' + PORT)
+    console.log(`Server running in  ${process.env.NODE_ENV} mode on port ${PORT}`)
 })
