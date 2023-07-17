30ed9173ce5fdd0b519c7db27cd3f3cbe5ac959c
Ivan Stojanovic <ivanstojanovic00@gmail.com>
2023-07-17 13:06:08 -0400

    testing db and storing, kkzcv pushed to collection

---
+++ models/playersData.js
@@ -0,0 +1,150 @@
+let playersData = [
+    {
+    "name": "Nemanja Nedovic",
+    "team": "Crvena Zvezda",
+    "position": ["SG"],
+    "height": 192,
+    "age": 32,
+    "jerseyNumber": 26
+    },
+    {
+    "name": "Facundo Campazzo",
+    "team": "Crvena Zvezda",
+    "position": ["PG"],
+    "height": 181,
+    "age": 32,
+    "jerseyNumber": 7
+    },
+    {
+    "name": "Luca Vildoza",
+    "team": "Crvena Zvezda",
+    "position": ["PG"],
+    "height": 191,
+    "age": 27,
+    "jerseyNumber": 1
+    },
+    {
+    "name": "Luka Mitrovic",
+    "team": "Crvena Zvezda",
+    "position": ["PF", "C"],
+    "height": 206,
+    "age": 30,
+    "jerseyNumber": 9
+    },
+    {
+    "name": "Filip Petrusev",
+    "team": "Crvena Zvezda",
+    "position": ["C"],
+    "height": 211,
+    "age": 23,
+    "jerseyNumber": 33
+    },
+    {
+    "name": "Ognjen Dobric",
+    "team": "Crvena Zvezda",
+    "position": ["SF"],
+    "height": 200,
+    "age": 28,
+    "jerseyNumber": 13
+    },
+    {
+    "name": "Branko Lazic",
+    "team": "Crvena Zvezda",
+    "position": ["PG"],
+    "height": 195,
+    "age": 34,
+    "jerseyNumber": 10
+    },
+    {
+    "name": "Stefan Markovic",
+    "team": "Crvena Zvezda",
+    "position": ["PG", "SG"],
+    "height": 198,
+    "age": 35,
+    "jerseyNumber": 27
+    },
+    {
+    "name": "Ben Bentil",
+    "team": "Crvena Zvezda",
+    "position": ["PF"],
+    "height": 206,
+    "age": 28,
+    "jerseyNumber": 50
+    },
+    {
+    "name": "Hassan Martin",
+    "team": "Crvena Zvezda",
+    "position": ["PF"],
+    "height": 201,
+    "age": 27,
+    "jerseyNumber": 12
+    },
+    {
+    "name": "John Holland",
+    "team": "Crvena Zvezda",
+    "position": ["SG", "SF"],
+    "height": 192,
+    "age": 32,
+    "jerseyNumber": 26
+    },
+    {
+    "name": "Jaylen Adams",
+    "team": "Crvena Zvezda",
+    "position": ["PG"],
+    "height": 188,
+    "age": 27,
+    "jerseyNumber": 3
+    },
+    {
+    "name": "Nikola Ivanovic",
+    "team": "Crvena Zvezda",
+    "position": ["SG"],
+    "height": 199,
+    "age": 33,
+    "jerseyNumber": 20
+    },
+    {
+    "name": "Dalibor Ilic",
+    "team": "Crvena Zvezda",
+    "position": ["PF"],
+    "height": 202,
+    "age": 23,
+    "jerseyNumber": 22
+    },
+    {
+    "name": "Ognjen Kuzmic",
+    "team": "Crvena Zvezda",
+    "position": ["C"],
+    "height": 213,
+    "age": 33,
+    "jerseyNumber": 32
+    },
+    {
+    "name": "Stefan Lazarevic",
+    "team": "Crvena Zvezda",
+    "position": ["SF", "PF"],
+    "height": 198,
+    "age": 26,
+    "jerseyNumber": 27
+    },
+    {
+    "name": "Miroslav Raduljica",
+    "team": "Crvena Zvezda",
+    "position": ["C"],
+    "height": 213,
+    "age": 35,
+    "jerseyNumber": 15
+    },
+    {
+    "name": "Nikola Topic",
+    "team": "Crvena Zvezda",
+    "position": ["PG", "SG"],
+    "height": 195,
+    "age": 17,
+    "jerseyNumber": 44
+    }
+]
+
+module.exports = playersData
+
+
--- server.js
+++ server.js
@@ -2,14 +2,43 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
+const cors = require('cors')
 const Player = require('./models/playerModel')
+const playersData = require('./models/playersData')
 // const bodyParser = require('body-parser')
 // PORT = 1991
+
 
 // Load config
 dotenv.config({path: './config/config.env'})
 
+//Handle CORS error
+app.use(cors())
+
 connectDB()
+
+const pushPlayers = async () => {
+    try {
+        for(let igrach of playersData){
+        //check if existing player already exists
+        const existingPlayer = await Player.findOne({ name: igrach.name });
+      if (existingPlayer) {
+        console.log(`Player '${igrach.name}' already exists. Skipping.`);
+        continue;
+      }
+
+        
+            const player = await Player.create(igrach);
+            console.log(`Created player: ${player.name}`)
+        }
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+pushPlayers()
+
+
 
 app.use(express.static('public'))
 app.use(express.json())
@@ -18,15 +47,15 @@ app.get('/', (req,res) => {
     res.send("Working")
 })
 
-app.post('/player', async (req,res) => {
-        try {
-             const player = await Player.create(req.body)
-             res.status(200).json(player)
-        } catch (error) {
-            console.error(error)
-            res.status(500).json({message: error.message})
-        }
-})
+// app.post('/player', async (req,res) => {
+//         try {
+//              const player = await Player.create(req.body)
+//              res.status(200).json(player)
+//         } catch (error) {
+//             console.error(error)
+//             res.status(500).json({message: error.message})
+//         }
+// })
 
 const PORT = process.env.PORT || 1991
 
