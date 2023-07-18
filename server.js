1aec4edd66ff3273bdc15dc81f98105e1a821182
Ivan Stojanovic <ivanstojanovic00@gmail.com>
2023-07-18 13:25:29 -0400

    setup public folder, testing autocomplete feature

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const connectDB = require('./config/db')
 const cors = require('cors')
 const Player = require('./models/playerModel')
 const playersData = require('./models/playersData')
+// const userInput = document.getElementById('userInput')
 // const bodyParser = require('body-parser')
 // PORT = 1991
 
-
+app.use(express.static('public'))
 // Load config
 dotenv.config({path: './config/config.env'})
 
@@ -23,11 +24,9 @@ const pushPlayers = async () => {
         //check if existing player already exists
         const existingPlayer = await Player.findOne({ name: igrach.name });
       if (existingPlayer) {
-        console.log(`Player '${igrach.name}' already exists. Skipping.`);
-        continue;
-      }
-
-        
+        // console.log(`Player '${igrach.name}' already exists. Skipping.`);
+        continue;``
+      } 
             const player = await Player.create(igrach);
             console.log(`Created player: ${player.name}`)
         }
@@ -39,13 +38,18 @@ const pushPlayers = async () => {
 pushPlayers()
 
 
-
-app.use(express.static('public'))
+// app.use(express.static('public'))
 app.use(express.json())
 
-app.get('/', (req,res) => {
-    res.send("Working")
-})
+// app.get('/', (req,res) => {
+//     res.sendFile(__dirname + '/public/index.html')
+// })
+// app.get('/test', async (req,res) => {
+//     res.sendFile(__dirname + '/views/index.html')
+//     const proba =   await Math.floor(Math.random() * playersData.length)
+// })
+
+
 
 // app.post('/player', async (req,res) => {
 //         try {
