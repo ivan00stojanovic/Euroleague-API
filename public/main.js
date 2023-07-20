65b761c0fc13c46a8f0e482c9aaecb474ac9de37
Ivan Stojanovic <ivanstojanovic00@gmail.com>
2023-07-20 12:45:06 -0400

    testing autocomplete feature

--- public/main.js
+++ public/main.js
@@ -1,6 +1,17 @@
 const input  = document.getElementById('player-input')
+const playersListElement = document.getElementById('players-list')
+
+let kkczv = []
 
 
+
+input.addEventListener("keypress", e => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    // fetchPlayers()
+    input.value = '';
+  }
+});
 
 const checkInput = (inputed, playerOTD) =>{
     // if(input.value === comparision) console.log('svakacast') 
@@ -9,45 +20,45 @@ const checkInput = (inputed, playerOTD) =>{
 }
 
 
-input.addEventListener("keypress", e => {
-    if (e.key === "Enter") {
-      e.preventDefault();
-      fetchPlayers()
-      input.value = '';
-    }
-  });
-
 const fetchPlayers = async () => {
     const inputedPlayer = input.value.toLowerCase()
     const response = await fetch('http://localhost:1991/getInfo')
     const data = await response.json()
+    console.log(data)
     // console.log(data[0].name.toLowerCase())
     checkInput(inputedPlayer, data[0].name.split(' ')[0].toLowerCase())
+     kkczv = data.map(players => players.name)
+     loadData(kkczv, playersListElement)
+     console.log(kkczv) 
      const randomOne =  data[Math.floor(Math.random() * data.length)]
 }
 
-// const fetchOne = async (playerName) => {
-//     try {
-//       const response = await fetch(`/api/players/${playerName}`);
-//       if (response.ok) {
-//         const player = await response.json();
-//         console.log(player);
-//       } else {
-//         console.error('Error:', response.status);
-//       }
-//     } catch (error) {
-//       console.error('Error fetching player:', error);
-//     }
-//   };
+fetchPlayers()
 
+const loadData = (data, element) => {
+  if(data){
+    element.innerHTML = '';
+    let innerElement = '';
+    data.forEach(player => {
+        innerElement += `
+        <li>${player}</li>`
+    });
+      element.innerHTML = innerElement;
+  }
+}
+
+const filterData = (data, searchText) => {
+ return  data.filter(player => player.includes(searchText));
+}
+
+input.addEventListener('input', () => {
+  const filteredData = filterData(kkczv, playersListElement.value)
+  loadData(filteredData, input)
+})
 
 
 // const getPlayerOfTheDay = () => {
 
 // }
 
-
 //   fetchOne('nemanja nedovic');
-
-
-console.log('duvaj ga')
