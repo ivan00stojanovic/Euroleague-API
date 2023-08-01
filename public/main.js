5a08a846116356d7cbec711bff3f8f2f31ff2b60
Ivan Stojanovic <ivanstojanovic00@gmail.com>
2023-08-01 17:48:08 -0400

    refactoring, playing with autocomplete && comparisions

--- public/main.js
+++ public/main.js
@@ -1,65 +1,53 @@
-const input  = document.getElementById('player-input')
-const playersListElement = document.getElementById('players-list')
+const input = document.getElementById('player-input');
+const playersListElement = document.getElementById('players-list');
+let kkczv = [];
+let randomOne = null;
 
-let kkczv = []
+const fetchPlayers = async () => {
+  const inputedPlayer = input.value.toLowerCase();
+  const response = await fetch('http://localhost:1991/getInfo');
+  const data = await response.json();
+  kkczv = data.map((players) => players.name);
+  loadData(kkczv, playersListElement);
+  randomOne = data[Math.floor(Math.random() * data.length)];
+  console.log(randomOne.name)
+};
 
+const loadData = (data, element) => {
+  if (data) {
+    element.innerHTML = '';
+    let innerElement = '';
+    data.forEach((player) => {
+      innerElement += `<li>${player}</li>`;
+    });
+    element.innerHTML = innerElement;
+  }
+};
 
+const filterData = (data, searchText) => {
+  return data.filter((player) => player.toLowerCase().includes(searchText.toLowerCase()));
+};
 
-input.addEventListener("keypress", e => {
-  if (e.key === "Enter") {
-    e.preventDefault();
-    // fetchPlayers()
-    input.value = '';
-    loadData(kkczv,playersListElement)
-    fetchPlayers()
+input.addEventListener('input', () => {
+  const filteredData = filterData(kkczv, input.value);
+  loadData(filteredData, playersListElement);
+});
+
+input.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    console.log('Enter key pressed! Input value:', input.value);
+    compareInput(input.value.toLowerCase(), randomOne.name.toLowerCase());
+    input.value = ''
   }
 });
 
-const checkInput = (inputed, playerOTD) =>{
-    // if(input.value === comparision) console.log('svakacast') 
-    console.log(inputed, playerOTD)
-    if(inputed === playerOTD) console.log('Inputs match')
-}
-
-
-const fetchPlayers = async () => {
-    const inputedPlayer = input.value.toLowerCase()
-    const response = await fetch('http://localhost:1991/getInfo')
-    const data = await response.json()
-    kkczv = data.map(players => players.name)
-    loadData(kkczv, playersListElement)
-    checkInput(inputedPlayer, kkczv[0])
-     const randomOne =  data[Math.floor(Math.random() * data.length)]
-}
-
-
-
-fetchPlayers()
-
-const loadData = (data, element) => {
-  if(data){
-    element.innerHTML = '';
-    let innerElement = '';
-    data.forEach(player => {
-        innerElement += `
-        <li>${player}</li>`
-    });
-      element.innerHTML = innerElement;
+const compareInput = (inputed, randomPlayerName) => {
+  if (inputed === randomPlayerName) {
+    console.log('Inputs match! Congratulations!');
+  } else {
+    console.log('Inputs do not match. Keep trying!');
   }
-}
+};
 
-const filterData = (data, searchText) => {
- return  data.filter(player => player.toLowerCase().includes(searchText.toLowerCase()));
-}
-
-input.addEventListener('input', () => {
-  const filteredData = filterData(kkczv, input.value)
-  loadData(filteredData, playersListElement)
-})
-
-
-// const getPlayerOfTheDay = () => {
-
-// }
-
-//   fetchOne('nemanja nedovic');
+// Fetch players when the page loads
+fetchPlayers();
