d4048d6a0d87f9a3862ddfd2f1e2ed06e309a97b
ivan00stojanovic <109870349+ivan00stojanovic@users.noreply.github.com>
2025-02-17 23:25:01 -0500

    Update main.js

--- public/main.js
+++ public/main.js
@@ -26,8 +26,8 @@ const answerContainer = document.querySelector('.table-container')
 
 
 const fetchPlayers = async () => {
-  const response = await fetch('https://eurole.onrender.com/api/players/all');
-
+  const response = await fetch('/api/players/all');
+  
   const data = await response.json();
   fetchArray = data.map((players) => players.name);
   allPlayersArray = data
